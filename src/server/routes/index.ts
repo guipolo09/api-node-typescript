aa2c383f4b4c37437b58d55752acf4080b0977f6
guipolo09 <guilhermemartinspolo@gmail.com>
2024-02-07 10:40:54 -0300

    feat: create controllers

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import { StatusCodes } from "http-status-codes";
+//import { StatusCodes } from "http-status-codes";
+
+import { CidadesController } from "../controllers/cidades";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
   return res.send("Olá, Dev!");
 });
 
-router.post("/test", (req, res) => {
-  console.log(req.body);
-  return res.status(StatusCodes.ACCEPTED).json(req.body);
-});
+router.post("/cidades", CidadesController.create);
 
 export { router };
