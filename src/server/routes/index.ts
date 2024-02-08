256d3c4354d6d8c9085848bafca77ea210123230
guipolo09 <guilhermemartinspolo@gmail.com>
2024-02-08 10:53:38 -0300

    feat: initial validations middlewares

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,6 +9,10 @@ router.get("/", (_, res) => {
   return res.send("Olá, Dev!");
 });
 
-router.post("/cidades", CidadesController.create);
+router.post(
+  "/cidades",
+  CidadesController.createBodyValidator,
+  CidadesController.create
+);
 
 export { router };
