d84f9b8c1feb97aa15007f5c2f03816948e1360e
guipolo09 <guilhermemartinspolo@gmail.com>
2024-02-09 11:00:17 -0300

    feat: config validation with errors

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,36 +1,31 @@
-import { Request, RequestHandler, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+import { Request, Response } from "express";
 import * as yup from "yup";
+
+import { validation } from "../../shared/middlewares";
 
 interface ICidade {
   nome: string;
   estado: string;
 }
+interface IFilter {
+  filter?: string;
+}
 
-const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
-  nome: yup.string().required().min(3),
-  estado: yup.string().required().min(3),
-});
-
-export const createBodyValidator: RequestHandler = async (req, res, next) => {
-  try {
-    await bodyValidation.validate(req.body, { abortEarly: false });
-    return next();
-  } catch (err) {
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
-
-    yupError.inner.forEach((error) => {
-      if (error.path === undefined) return;
-      errors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
-  }
-};
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICidade>(
+    yup.object().shape({
+      nome: yup.string().required().min(3),
+      estado: yup.string().required().min(3),
+    })
+  ),
+  query: getSchema<IFilter>(
+    yup.object().shape({
+      filter: yup.string().required.min(3),
+    })
+  ),
+}));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   console.log(req.body);
-
-  return res.send("Created!");
+  return res.send("Created!!");
 };
