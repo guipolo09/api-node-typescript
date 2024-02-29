81095407185a3e9f66577ffbd806cd1f2e87bec4
martinspolo <103054170+guipolo09@users.noreply.github.com>
2024-02-29 12:39:52 -0300

    update create pessoa

--- src/server/database/migrations/0001_create_pessoa.ts
+++ src/server/database/migrations/0001_create_pessoa.ts
@@ -10,7 +10,7 @@ export async function up(knex: Knex) {
       table.bigIncrements('id').primary().index();
       table.string('nomeCompleto').index().notNullable();
       table.string('email').unique().notNullable()
-      table.bigInteger('cidadeId').index().notNullable().references('id').inTable(ETableNames.cidade)
+      table.bigInteger('cidadeId').index().notNullable().references('id').inTable(ETableNames.cidade).onUpdate('CASCADE').onDelete('RESTRICT')
 
       table.comment('Tabela usada para armazenar pessoas do sistema.');
     })
