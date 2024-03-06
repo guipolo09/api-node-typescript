1b61f511adb5c9222c7e2c1c33971f0ab5bd4800
martinspolo <103054170+guipolo09@users.noreply.github.com>
2024-03-06 13:18:04 -0300

    feat? jwt config

--- src/server/controllers/pessoas/GetAll.ts
+++ src/server/controllers/pessoas/GetAll.ts
@@ -23,6 +23,8 @@ export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Respons
   const result = await PessoasProvider.getAll(req.query.page || 1, req.query.limit || 7, req.query.filter || '');
   const count = await PessoasProvider.count(req.query.filter);
 
+  console.log("id do usuario", req.headers.idUsuario)
+
   if (result instanceof Error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       errors: { default: result.message }
--- src/server/shared/middlewares/EnsureAuthenticated.ts
+++ src/server/shared/middlewares/EnsureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import { StatusCodes } from "http-status-codes";
+import { JWTService } from "../services";
 
 
 
@@ -15,7 +16,7 @@ export const ensureAuthenticated: RequestHandler = async ( req, res, next) => {
     } 
 
     const [type, token] = authorization.split(" ")
-    console.log(type, token)
+    //console.log(type, token)
     
     if (type !== "Bearer") {
         return res.status(StatusCodes.UNAUTHORIZED).json({
@@ -25,12 +26,22 @@ export const ensureAuthenticated: RequestHandler = async ( req, res, next) => {
         })
     } 
     
-    if (token !== "teste.teste.teste") {
+    const jwtData = JWTService.verify(token)
+    if (jwtData === "JWT_SECRET_NOT_FOUND") {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            errors: {
+                default: 'Erro ao verificar token'
+            }
+        })
+    } else if (jwtData === "INVALID_TOKEN") {
         return res.status(StatusCodes.UNAUTHORIZED).json({
             errors: {
                 default: 'Token inválido'
             }
         })
     }
+
+    //console.log(jwtData)
+    req.headers.idUsuario = jwtData.uid.toString()
     return next()
 }
